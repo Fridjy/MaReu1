396b77de587247e0d64c8a184606cf452d389ab0
Fridjy <fridjycompere8@gmail.com>
2022-12-17 02:17:47 -0500

    Initial commit

---
+++ Meeting.java
@@ -0,0 +1,47 @@
+package com.openclassroom.mareu.model;
+
+public class Meeting {
+    private String name;
+    private String time;
+    private String place;
+    private String mails;
+
+    public Meeting(String sujet, String time, String place, String mails){
+        this.name = name;
+        this.time = time;
+        this.place = place;
+        this.mails = mails;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getMails() {
+        return mails;
+    }
+
+    public void setMails(String mails) {
+        this.mails = mails;
+    }
+}
